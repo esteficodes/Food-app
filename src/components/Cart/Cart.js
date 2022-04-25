88aef702a68481304e0f38eaa3e60d0f0bb144fd
esteficodes <estefaniaquevedolusby@gmail.com>
2022-04-25 15:38:58 +0200

    added logic for cart states

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,9 @@
-import Modal from '../UI/Modal';
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartItems = (
-    <ul classname={classes["cart-items"]}>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
@@ -11,15 +11,18 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={classes.actions}></div>
-      <button className={['button--alt']}>Close</button>
-      <button className={classes.button}>Order</button>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
+      </div>
     </Modal>
   );
 };
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,13 +2,15 @@ import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-    return <button className={classes.button}>
+    return (
+      <button className={classes.button} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
         <span>Your cart</span>
         <span className={classes.badge}>3</span>
-    </button>
+      </button>
+    );
 };
 
 export default HeaderCartButton;
